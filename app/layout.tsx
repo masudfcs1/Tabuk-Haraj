6e9f347fc4f4dc7846c97f36fecbccb99c2efb43
masudfcs1 <masudfcs1@gmail.com>
2025-12-11 14:04:37 +0600

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Analytics } from "@vercel/analytics/next"
-import "./../app/globals.css"
+import "../../../styles/globals.css"
 
 export const metadata: Metadata = {
   title: {
