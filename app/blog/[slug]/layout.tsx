ee1fb6b9917c3c046614a1f76a05de03dd979164
masudfcs1 <masudfcs1@gmail.com>
2025-12-11 14:02:00 +0600

    gobla css issue fixed

--- app/blog/[slug]/layout.tsx
+++ app/blog/[slug]/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Analytics } from "@vercel/analytics/next"
-import "./globals.css"
+import "./../app/globals.css"
 
 export const metadata: Metadata = {
   title: {
